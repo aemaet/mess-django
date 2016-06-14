b7e6701fd60e570c930cfe8602ea8ba607af4910
aemaet <darlinard@gmail.com>
2016-06-14 17:59:30 +0300

    fixed order

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 from .models import Post, Comment
 
 def post_list(request):
-    post_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    post_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     paginator = Paginator(post_list, 5)
     page = request.GET.get('page')
     try:
